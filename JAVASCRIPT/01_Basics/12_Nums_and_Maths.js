baeae9927e770de5f95fcdc59124b644e6ac9180
Harshvardhan-00012257 <136665792+Harshvardhan-00012257@users.noreply.github.com>
2024-02-21 15:23:48 +0530

    Maths and Num in JS

--- JAVASCRIPT/01_Basics/12_Nums_and_Maths.js
+++ JAVASCRIPT/01_Basics/12_Nums_and_Maths.js
@@ -75,3 +75,17 @@ console.log(" ")
 console.log(" ")
 console.log("Maths In JS (library)")
 console.log(Math)  //Object [Math] {}  ->  Math ek object hai with {} (i.e. many properties)
+console.log(Math.abs(-4))
+console.log(Math.round(4.4))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.8))
+console.log(Math.min(4.8, 9 ,10, 2.25))
+console.log(Math.max(4.8, 9 ,10, 2.25))
+
+console.log(Math.random())      //Always outputs value betn 0 and 1
+console.log((Math.random()*10) + 1)      //Always outputs value betn 1 and 10
+console.log(Math.floor((Math.random()*10) + 1))      //Always outputs integer value from 1 to 10
+
+const min = 10
+const max = 20
+console.log( Math.floor( (Math.random()) * (max - min + 1) ) + min )   // Always outputs integer value from min to max
